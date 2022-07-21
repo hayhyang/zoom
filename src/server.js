e9448cd9d74a516b73c2480af66c6cf2e5d1680e
Mihyang Seo <mihyangseo@Mihyangui-MacBookPro.local>
2022-07-21 18:47:25 +0900

    WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,6 +9,9 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const handleListen = () => console.log(`Listening on ws://localhost:3000`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const ws = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
